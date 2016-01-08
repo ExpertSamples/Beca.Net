6f41ea02ec97d2476f0b07619b72c37f649f0812
Antonio Marín <antonio.marin@expertscoding.es>
2016-01-08 15:26:06 +0100

    Clase Parcial Empleado

---
+++ Ejemplos.DAO/DataSets/Empleados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplos.DAO.DataSets
+{
+    partial class Empleados
+    {
+        partial class PersonRow
+        {
+            public string FullName
+            {
+                get
+                {
+                    return this.FirstName + " " + this.LastName;
+                }
+            }
+        }
+
+    }
+}
